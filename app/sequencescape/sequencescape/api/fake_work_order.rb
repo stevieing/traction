b7cb2d77fa3dc84c96681a66e351195eab1ba44c
Stephen Inglis <si5@sanger.ac.uk>
2018-05-18 09:01:14 +0100

    Adde work order support for reception page.

--- app/sequencescape/sequencescape/api/fake_work_order.rb
+++ app/sequencescape/sequencescape/api/fake_work_order.rb
@@ -5,17 +5,70 @@ module Sequencescape
     # this class is used if sequencescape connection is disabled
     # see config/initializers/sequencescape_api.rb and config/sequencescape.yml
     class FakeWorkOrder
+
+      WORK_ORDER_COUNT = 10
+
       def self.for_reception
-        []
+        [].tap do |work_orders|
+          (1..10).each do |i|
+            work_orders << SequencescapeWorkOrder.new(id: 1000+i)
+          end
+        end
       end
 
-      def self.find_by_ids(_ids)
-        []
+      def self.find_by_ids(ids)
+        ids.collect do |id|
+          SequencescapeWorkOrder.new(id: id)
+        end
       end
 
       def self.update_state(_work_order)
         true
       end
+
+      class SequencescapeWorkOrder
+        include ActiveModel::Model
+
+        attr_accessor :id
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, 'WorkOrder')
+        end
+
+        def persisted?
+          false
+        end
+
+        def study_uuid
+          SecureRandom.uuid
+        end
+
+        def sample_uuid
+          SecureRandom.uuid
+        end
+
+        def library_preparation_type
+          'rapid'
+        end
+
+        def state
+          'pending'
+        end
+
+        def data_type
+          'basecalls'
+        end
+
+        def number_of_flowcells
+          3
+        end
+
+        def name
+          "DN#{id}A:A1"
+        end
+      end
+
     end
+
   end
 end
--- spec/sequencescape/api/fake_work_order_spec.rb
+++ spec/sequencescape/api/fake_work_order_spec.rb
@@ -3,9 +3,65 @@
 require 'rails_helper'
 
 RSpec.describe Sequencescape::Api::FakeWorkOrder, type: :model do
-  it 'should not reach Sequencescape if FakeWorkOrderWork id used' do
-    expect(Sequencescape::Api::FakeWorkOrder.for_reception).to eq []
-    expect(Sequencescape::Api::FakeWorkOrder.find_by_ids(%w[1 2])).to eq []
-    expect(Sequencescape::Api::FakeWorkOrder.update_state('test')).to eq true
+  # it 'should not reach Sequencescape if FakeWorkOrderWork id used' do
+  #   expect(Sequencescape::Api::FakeWorkOrder.for_reception).to eq []
+  #   expect(Sequencescape::Api::FakeWorkOrder.find_by_ids(%w[1 2])).to eq []
+  #   expect(Sequencescape::Api::FakeWorkOrder.update_state('test')).to eq true
+  # end
+
+  it 'creates a number of fake work orders' do
+    work_orders = Sequencescape::Api::FakeWorkOrder.for_reception
+    expect(work_orders.length).to eq(Sequencescape::Api::FakeWorkOrder::WORK_ORDER_COUNT)
+  end
+
+  it 'finds fake work orders by id' do
+    work_orders = Sequencescape::Api::FakeWorkOrder.find_by_ids([1000, 10001, 10002])
+    expect(work_orders.length).to eq(3)
+    expect(work_orders.first.id).to eq(1000)
+  end
+
+  describe "SequencescapeWorkOrder" do
+
+    it 'has a model name' do
+      expect(Sequencescape::Api::FakeWorkOrder::SequencescapeWorkOrder.model_name).to eq('WorkOrder')
+    end
+
+    it 'is never persisted' do
+      expect(Sequencescape::Api::FakeWorkOrder::SequencescapeWorkOrder.new).to_not be_persisted
+    end
+
+    describe '#new' do
+      let(:work_order)  { Sequencescape::Api::FakeWorkOrder::SequencescapeWorkOrder.new(
+                            id: 1
+                        )}
+
+      it 'has an id' do
+        expect(work_order.id).to eq(1)
+      end
+
+      it 'has a name' do
+        expect(work_order.name).to eq('DN1A:A1')
+      end
+
+      it 'has state' do
+        expect(work_order.state).to eq('pending')
+      end
+
+      it 'has a data type' do
+        expect(work_order.data_type).to eq('basecalls')
+      end
+
+      it 'has a number of flowcells' do
+        expect(work_order.number_of_flowcells).to eq(3)
+      end
+
+      it 'has a sample uuid' do
+        expect(work_order.sample_uuid).to be_present
+      end
+
+       it 'has a study uuid' do
+        expect(work_order.study_uuid).to be_present
+      end
+    end
   end
 end
